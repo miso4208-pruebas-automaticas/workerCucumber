b79bdf9d14b830fc93ca45cf8cbc4368c5b2198c
alejitro <alejitro@gmail.com>
2019-11-21 20:52:47 -0500

    correcion numero de ejecuciones test

--- app/services/cucumber.srv.js
+++ app/services/cucumber.srv.js
@@ -1,5 +1,6 @@
 'use strict'
 const shell = require('shelljs');
+const request = require('request');
 const fs = require('fs');
 const s3 = require('../../worker-sqs/s3Storage.js')
 
@@ -10,23 +11,34 @@ module.exports.generateCucumber = function(req,success,error){
     let code = req.code;
     const codeinit = req.code;
     var item = 1;
-    shell.exec('./node_modules/.bin/wdio wdio.conf.js', function(stdout, stderr) {
-        fs.readdir(`${path}/test/report`,function(err, items) {
-            let file;
-            for(var i=0;i<items.length;i++){
-                if(items[i].includes('html')){
-                    file = items[i];
-                    break;
-                }
-            }
-            const content = fs.readFileSync(`${path}/test/report/${file}`);
-            s3.saveFileToS3(`${code}`,content,()=>{
-                for(var i=0;i<items.length;i++){
-                    if(items[i].includes('html')){
-                        fs.unlinkSync(`${path}/test/report/${items[i]}`);
+    for(var i = 0,p = Promise.resolve();i<itemsEx;i++){
+        p= p.then(_ => new Promise(resolve => {
+            code = `${codeinit}_${item}`;
+            shell.exec('./node_modules/.bin/wdio wdio.conf.js', function(stdout, stderr) {
+                fs.readdir(`${path}/test/report`,function(err, items) {
+                    let file;
+                    for(var i=0;i<items.length;i++){
+                        if(items[i].includes('html')){
+                            file = items[i];
+                            break;
+                        }
                     }
-                }
+                    const content = fs.readFileSync(`${path}/test/report/${file}`);
+                    s3.saveFileToS3(`${code}`,content,()=>{
+                        for(var i=0;i<items.length;i++){
+                            if(items[i].includes('html')){
+                                fs.unlinkSync(`${path}/test/report/${items[i]}`);
+                            }
+                        }
+                        if(item == itemsEx){
+                            success("ok");
+                        }else{
+                            item = item+1;
+                            resolve();
+                        }
+                    });
+                });
             });
-        });
-    });
+        }));
+    }
 }
